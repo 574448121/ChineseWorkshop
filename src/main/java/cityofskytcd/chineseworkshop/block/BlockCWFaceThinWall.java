251fb78d43eeb5dae8d08fd9b3a9dbb367a22667
Snownee <1850986885@qq.com>
2018-06-03 11:21:35 +0800

    Clean up

--- src/main/java/cityofskytcd/chineseworkshop/block/BlockCWFaceThinWall.java
+++ src/main/java/cityofskytcd/chineseworkshop/block/BlockCWFaceThinWall.java
@@ -12,17 +12,12 @@ import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyDirection;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
-import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
 /**
  * 朝向薄墙类
