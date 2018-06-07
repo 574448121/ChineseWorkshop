3f9fed6d1912238bab62e8209d196e8bda5e6726
Snownee <1850986885@qq.com>
2018-06-08 05:00:55 +0800

    Fix wrong BlockShape

--- src/main/java/cityofskytcd/chineseworkshop/block/BlockCWThreshold.java
+++ src/main/java/cityofskytcd/chineseworkshop/block/BlockCWThreshold.java
@@ -9,9 +9,7 @@
 package cityofskytcd.chineseworkshop.block;
 
 import net.minecraft.block.material.Material;
-import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
@@ -31,11 +29,5 @@ public class BlockCWThreshold extends BlockCWTFace2D
     public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos)
     {
         return new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.5D, 1.0D);
-    }
-
-    @Override
-    public BlockFaceShape getBlockFaceShape(IBlockAccess worldIn, IBlockState state, BlockPos pos, EnumFacing face)
-    {
-        return face == EnumFacing.DOWN ? BlockFaceShape.SOLID : BlockFaceShape.UNDEFINED;
     }
 }
