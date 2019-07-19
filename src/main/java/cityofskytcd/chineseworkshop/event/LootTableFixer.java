9c9a5dd134e43d827fd091531394c91b88ea04d8
Snownee <mjy523@126.com>
2019-07-19 21:28:12 +0800

    Fix loot tables

---
+++ src/main/java/cityofskytcd/chineseworkshop/event/LootTableFixer.java
@@ -0,0 +1,16 @@
+package cityofskytcd.chineseworkshop.event;
+
+import net.minecraftforge.event.LootTableLoadEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+
+@EventBusSubscriber(bus = Bus.MOD)
+public class LootTableFixer
+{
+    @SubscribeEvent
+    public static void onLoad(LootTableLoadEvent event)
+    {
+        //System.out.println(event.getName());
+    }
+}
