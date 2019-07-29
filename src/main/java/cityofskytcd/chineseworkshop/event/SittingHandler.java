993797a49f2125d4250ec30dc8acb1246aca3083
Snownee <mjy523@126.com>
2019-07-29 23:09:15 +0800

    Port to 1.14.4. Fix Forge breaking block models

--- src/main/java/cityofskytcd/chineseworkshop/event/SittingHandler.java
+++ src/main/java/cityofskytcd/chineseworkshop/event/SittingHandler.java
@@ -19,7 +19,7 @@ import net.minecraft.util.ActionResultType;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
-import net.minecraft.world.ServerWorld;
+import net.minecraft.world.World;
 import net.minecraftforge.common.util.FakePlayer;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -31,7 +31,7 @@ public class SittingHandler
     @SubscribeEvent
     public static void onRightClickBlock(PlayerInteractEvent.RightClickBlock event)
     {
-        if (!(event.getWorld() instanceof ServerWorld))
+        if (event.getWorld().isRemote)
         {
             return;
         }
@@ -45,7 +45,7 @@ public class SittingHandler
         if (!stack1.isEmpty() || !stack2.isEmpty())
             return;
 
-        ServerWorld world = (ServerWorld) event.getWorld();
+        World world = event.getWorld();
         BlockPos pos = event.getPos();
 
         Vec3d vec = new Vec3d(pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5);
@@ -69,7 +69,7 @@ public class SittingHandler
             {
                 Vec3d v = iseat.getSeat(state, world, pos);
                 Seat seat = new Seat(world, v.add(pos.getX(), pos.getY(), pos.getZ()));
-                world.summonEntity(seat);
+                world.addEntity(seat);
                 player.startRiding(seat);
                 event.setCanceled(true);
                 event.setCancellationResult(ActionResultType.SUCCESS);
