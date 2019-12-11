1e1b0431d19617b41f4f9bf72cde866fc168a7c8
Snownee <1850986885@qq.com>
2019-12-11 12:38:39 +0800

    Fix packet handling

--- src/main/java/cityofskytcd/chineseworkshop/network/WheelMovePacket.java
+++ src/main/java/cityofskytcd/chineseworkshop/network/WheelMovePacket.java
@@ -12,52 +12,46 @@ import net.minecraft.network.PacketBuffer;
 import net.minecraftforge.fml.network.NetworkEvent.Context;
 import snownee.kiwi.network.ClientPacket;
 
-public class WheelMovePacket extends ClientPacket
-{
+public class WheelMovePacket extends ClientPacket {
     private int index;
 
-    public WheelMovePacket(int index)
-    {
+    public WheelMovePacket(int index) {
         this.index = index;
     }
 
-    public static class Handler extends PacketHandler<WheelMovePacket>
-    {
+    public static class Handler extends PacketHandler<WheelMovePacket> {
 
         @Override
-        public WheelMovePacket decode(PacketBuffer buf)
-        {
+        public WheelMovePacket decode(PacketBuffer buf) {
             return new WheelMovePacket(buf.readInt());
         }
 
         @Override
-        public void encode(WheelMovePacket pkt, PacketBuffer buf)
-        {
+        public void encode(WheelMovePacket pkt, PacketBuffer buf) {
             buf.writeInt(pkt.index);
         }
 
         @Override
-        public void handle(WheelMovePacket pkt, Supplier<Context> ctx)
-        {
-            ServerPlayerEntity player = ctx.get().getSender();
-            ItemStack held = player.getHeldItemMainhand();
-            if (held.isEmpty() || pkt.index < 0)
-            {
-                return;
-            }
-            Item item = held.getItem();
-            Selection selection = Selections.find(item);
-            if (selection == null)
-            {
-                return;
-            }
-            if (selection.matches(item))
-            {
-                ItemStack stack = new ItemStack(selection.get(pkt.index % selection.size()));
-                stack.setCount(held.getCount());
-                stack.setTag(held.getTag());
-                player.setItemStackToSlot(EquipmentSlotType.MAINHAND, stack);
-            }
+        public void handle(WheelMovePacket pkt, Supplier<Context> ctx) {
+            ctx.get().enqueueWork(() -> {
+                ServerPlayerEntity player = ctx.get().getSender();
+                ItemStack held = player.getHeldItemMainhand();
+                if (held.isEmpty() || pkt.index < 0) {
+                    return;
+                }
+                Item item = held.getItem();
+                Selection selection = Selections.find(item);
+                if (selection == null) {
+                    return;
+                }
+                if (selection.matches(item)) {
+                    ItemStack stack = new ItemStack(selection.get(pkt.index % selection.size()));
+                    stack.setCount(held.getCount());
+                    stack.setTag(held.getTag());
+                    player.setItemStackToSlot(EquipmentSlotType.MAINHAND, stack);
+                }
+            });
+            ctx.get().setPacketHandled(true);
         }
 
     }
